34a58d87c2624c505679d17633277ac9ad5e50a3
Justin Sitter <justin@jaytria.com>
2020-11-27 16:29:11 -0800

    Update to Drupal 7.75

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ -x "$(command -v docker)" ]; then
-    ./package_files/unpacker.py drupal-7.74.tar.gz ../drupal-src -d -r
+    ./package_files/unpacker.py drupal-7.75.tar.gz ../drupal-src -d -r
     cp ./drupal-src/sites/default/default.settings.php ./drupal-src/sites/default/settings.php
     chmod 755 ./drupal-src/sites/default/settings.php
     mv ./drupal-src/sites ./sites
